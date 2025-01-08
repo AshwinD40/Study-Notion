d36daec542c41a9f72554a926be4966c9aa8e93f
Ashwin Chaudhary <ashwinkumarchaudhary950@gmail.com>
2025-01-08 16:25:51 +0530

    Review slider fixing

--- src/components/common/ReviewSlider.jsx
+++ src/components/common/ReviewSlider.jsx
@@ -33,62 +33,63 @@ const ReviewSlider = () => {
     <div className=' text-white '>
         <div className=' my-[50px] h-[184px] max-w-maxContentTab lg:max-w-maxContent'>
             <Swiper
-                slidesPerView={3}
-                spaceBetween={24}
+                slidesPerView={4}
+                spaceBetween={25}
                 loop={true}
                 freeMode={true}
                 autoplay={
-                    {delay:1500, disableOnInteraction:false}
+                    {delay:2500, disableOnInteraction:false}
                 }
                 modules={[FreeMode,Autoplay, Pagination]}
                 className='w-full '
             >
                 {
-                    reviews.map((review , index) =>(
-                        <SwiperSlide key={index} className='flex flex-col gap-3  p-3 bg-richblack-800 rounded-lg'>
-                            <div className=' flex gap-4 items-center '>
-                                <img  
-                                    src = {review?.user?.image 
+                    reviews.map((review , index) =>{
+                        return (
+                        <SwiperSlide key={index} >
+                            <div className=' flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25 '>
+                                <div className=' flex items-center gap-4'>
+                                    <img  
+                                        src = {review?.user?.image 
                                             ? review?.user?.image 
                                             : `https://api.dicebear.com/5.x/initials/svg?seed=${review.user.firstName} 
                                             ${review.user.lastName}`
                                         }
-                                    alt=' Profile'
-                                    className=' h-9 w-9 rounded-full object-cover'
-
-                                />
-                                <div className=' flex flex-col'>
-                                    <h1 className=' text-richblack-25 font-semibold'>{review?.user?.firstName} {review?.user?.lastName}</h1>
-                                    <p className=' text-sm font-semibold text-richblack-600'>{review?.user?.email}</p>
-                                </div>
-                            </div>
-                            <p className=' text-caribbeangreen-400'>{review?.course?.courseName}</p>
-                            <p className=' text-richblack-100'>
-                                {review?.review.split(" ").length>
-                                truncateWords
-                                ? `${review.review
-                                    .split(" ")
-                                    .slice(0, truncateWords)
-                                    .join(" ")} ...`
+                                        alt=' Profile'
+                                        className=' h-9 w-9 rounded-full object-cover'
+                                    />
+                                    <div className=' flex flex-col'>
+                                        <h1 className=' text-richblack-5 font-semibold'>{review.user.firstName} {review.user.lastName}</h1>
+                                        <h2 className=' text-[12px] font-medium text-richblack-500'>{review?.course?.courseName}</h2>
+                                    </div>
+                                </div>  
+                                <p className=' font-medium text-richblack-25'>
+                                    {review?.review.split(" ").length>
+                                    truncateWords
+                                    ? `${review.review
+                                        .split(" ")
+                                        .slice(0, truncateWords)
+                                        .join(" ")} ...`
                                     : `${review.review}`}
-                            </p>
-                            <div className=' flex items-center gap-3'>
-                                <h3 className='text-[20px] text-yellow-100'>
-                                    {review?.rating.toFixed(1)} 
-                                </h3>
-                                <ReactStars 
-                                    count={5}
-                                    value={review?.rating}
-                                    size={24}
-                                    edit={false}
-                                    activeColor="#ffd700"
-                                    emptyIcon={<FaStar />}
-                                    fullIcon={<FaStar />}
-                                />
-                            </div>
+                                </p>
+                                <div className=' flex items-center gap-2'>
+                                    <h3 className=' font-semibold text-yellow-100'>
+                                        {review?.rating.toFixed(1)} 
+                                    </h3>
+                                    <ReactStars 
+                                        count={5}
+                                        value={review?.rating}
+                                        size={24}
+                                        edit={false}
+                                        activeColor="#ffd700"
+                                        emptyIcon={<FaStar />}
+                                        fullIcon={<FaStar />}
+                                    />
+                                </div>
+                            </div>   
                         </SwiperSlide>
-                    ))
-                }
+                        )
+                    })}
             
             </Swiper>
         </div>
